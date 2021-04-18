3b8237bbd3a3ec56199038f5d436e0f570c1b8ba
Sentimental Analyses on youtube <ishank.jain_cs18@gla.ac.in>
2021-04-18 12:15:37 +0530

    commit

--- main.py
+++ main.py
@@ -1,46 +1,19 @@
 from collections import Counter
 import youtubecomments as ytc
 from wordcloud import wordcloud
-
-googleapikey="AIzaSyAwXKPmjC4WUBknPhLLWezOYoOaowYdujI"
-
-
-
-inp = input("Please Enter Video ID: ")
-videoid = inp
-
-outputformat = "dataframe"
-
-data = ytc.get_comments(googleapikey, videoid, outputformat)
-
-cs = input("wohoo you did it enter the Filename: ")
-
-data.to_csv(cs,index = False)
-
 import pandas as pd
-df = pd.read_csv(cs)
 import matplotlib.pyplot as plt
-title_words = list(df["textDisplay"].apply(lambda x: x.split()))
-title_words = [x for y in title_words for x in y]
-print(Counter(title_words).most_common(25))
-wc = wordcloud.WordCloud(width=1200, height=500,
-                         collocations=False, background_color="white",
-                         colormap="tab20b").generate(" ".join(title_words))
-plt.figure(figsize=(15,10))
-plt.imshow(wc, interpolation="bilinear")
-_ = plt.axis("off")
-plt.show()
-
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 def sentiment_scores(sentence):
     sid_obj = SentimentIntensityAnalyzer()
     sentiment_dict = sid_obj.polarity_scores(sentence)
 
-    print("Overall sentiment dictionary is: ", sentiment_dict)
-    print("sentences was rated as ", sentiment_dict['neg'] * 100, "% Negative")
-    print("sentences was rated as ", sentiment_dict['neu'] * 100, "% Neutral")
-    print("sentences was rated as ", sentiment_dict['pos'] * 100, "% Positive")
+    print("*"*100,"\n")
+    print(sentence,"\n")
+    print("sentences was rated as ==>", sentiment_dict['neg'] * 100, "% Negative")
+    print("sentences was rated as ==>", sentiment_dict['neu'] * 100, "% Neutral")
+    print("sentences was rated as ==>", sentiment_dict['pos'] * 100, "% Positive")
 
     print("Sentence Overall rated as", end=" ")
 
@@ -50,20 +23,42 @@ def sentiment_scores(sentence):
         print("Negative")
     else:
         print("Neutral")
+    print("*"*100,"\n\n\n")
+
+
+googleapikey="AIzaSyAwXKPmjC4WUBknPhLLWezOYoOaowYdujI"
+
+videoid = input("Please Enter Video ID: ")
+
+outputformat = "dataframe"
+
+data = ytc.get_comments(googleapikey, videoid, outputformat)
+
+cs = "result"
+
+data.to_csv(cs,index = True)
+
+df = pd.read_csv(cs)
+df = df.sort_values(by=['publishedAt'],ascending=True)
+title_words = list(df["textOriginal"].apply(lambda x: x.split()))
+title_words = [x for y in title_words for x in y]
+wc = wordcloud.WordCloud(width=1200, height=500,
+                         collocations=False, background_color="Azure",
+                         colormap="viridis").generate(" ".join(title_words))
+plt.figure(figsize=(15,10))
+plt.imshow(wc, interpolation="nearest")
+_ = plt.axis("off")
+plt.show()
+
 
 
 if __name__ == "__main__":
     import pandas as pd
-    dataset = pd.read_csv(cs)
-    print(dataset.head())
-    print(len(dataset))
-    feature_names = ['textDisplay']
-    X = dataset[feature_names]
 
-    print("x:\n", X)
-    X.to_csv("comments.csv", index=False)
+    dataset = pd.read_csv(cs)
+    X = dataset['textDisplay']
+    X.to_csv("comments.csv", index=True)
     re = pd.read_csv("comments.csv")
 
     for i in re['textDisplay']:
-        print(i)
         sentiment_scores(i)

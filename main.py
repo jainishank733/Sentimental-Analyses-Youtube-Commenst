e044a129bf2c271b0f91bb56d6c936bfe19461c0
Sentimental Analyses on youtube <ishank.jain_cs18@gla.ac.in>
2021-04-20 23:47:04 +0530

    commit

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
-from collections import Counter
 import youtubecomments as ytc
 from wordcloud import wordcloud
 import pandas as pd
 import matplotlib.pyplot as plt
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from colorama import Fore, Back, Style
+
 
 def sentiment_scores(sentence):
     sid_obj = SentimentIntensityAnalyzer()
     sentiment_dict = sid_obj.polarity_scores(sentence)
 
-    print("*"*100,"\n")
-    print(sentence,"\n")
-    print("sentences was rated as ==>", sentiment_dict['neg'] * 100, "% Negative")
-    print("sentences was rated as ==>", sentiment_dict['neu'] * 100, "% Neutral")
-    print("sentences was rated as ==>", sentiment_dict['pos'] * 100, "% Positive")
+    print(Fore.LIGHTBLACK_EX+"*"*100)
+    print(Back.LIGHTCYAN_EX+sentence+Style.RESET_ALL,"\n")
+    print(Fore.RED+"sentences was rated as ==>", sentiment_dict['neg'] * 100, "% Negative")
+    print(Fore.YELLOW+"sentences was rated as ==>", sentiment_dict['neu'] * 100, "% Neutral")
+    print(Fore.GREEN+"sentences was rated as ==>", sentiment_dict['pos'] * 100, "% Positive")
 
-    print("Sentence Overall rated as", end=" ")
+    print(Fore.LIGHTBLUE_EX+"\nSentence Overall rated as", end=" ")
 
     if sentiment_dict['compound'] >= 0.05:
-        print("Positive")
+        print(Fore.GREEN+"Positive")
     elif sentiment_dict['compound'] <= -0.05:
-        print("Negative")
+        print(Fore.RED+"Negative")
     else:
-        print("Neutral")
-    print("*"*100,"\n\n\n")
-
+        print(Fore.YELLOW+"Neutral")
+    print(Fore.LIGHTBLACK_EX+"*"*100,)
+    print(Style.RESET_ALL,"\n")
 
 googleapikey="AIzaSyAwXKPmjC4WUBknPhLLWezOYoOaowYdujI"
 
@@ -39,8 +40,7 @@ cs = "result"
 data.to_csv(cs,index = True)
 
 df = pd.read_csv(cs)
-df = df.sort_values(by=['publishedAt'],ascending=True)
-title_words = list(df["textOriginal"].apply(lambda x: x.split()))
+title_words = list(df["textDisplay"].apply(lambda x: x.split()))
 title_words = [x for y in title_words for x in y]
 wc = wordcloud.WordCloud(width=1200, height=500,
                          collocations=False, background_color="Azure",
